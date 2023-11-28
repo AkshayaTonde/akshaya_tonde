6d20d834dbf4b399484f5e6214ecdfd0bb6fe768
Akshaya Tonde <tondeakshaya@gmail.com>
2023-11-28 12:00:34 -0800

    Home page

--- akshaya_tonde/akshaya_tonde/urls.py
+++ akshaya_tonde/akshaya_tonde/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from contact_management.views import *
+
 
 urlpatterns = [
+    path('', home, name= 'Home'),
     path('admin/', admin.site.urls),
 ]
