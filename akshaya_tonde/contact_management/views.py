4ec13d3cd6036756864ce72fe824d8bbfdc08ef0
Akshaya Tonde <tondeakshaya@gmail.com>
2023-11-28 17:56:22 -0800

    home page tab;e

--- akshaya_tonde/contact_management/views.py
+++ akshaya_tonde/contact_management/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, "index.html")
+
+    contact_list =[
+        {'Contact_name':'Akshaya', 'email':'akshaya@gmail.com','CreatedTime':'021545'},
+        {'Contact_name':'Mahan', 'email':'mohan@test.com','CreatedTime':'021545'},
+        {'Contact_name':'Swapnil', 'email':'swaps@test.com','CreatedTime':'021545'}
+
+    ]
+
+    return render(request, "index.html", context={'contact_list': contact_list})
 
 
