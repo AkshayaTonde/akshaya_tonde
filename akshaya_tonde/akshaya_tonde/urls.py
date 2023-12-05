c8c26424cee76bc899c238facc656743bb9be0b6
Akshaya Tonde <tondeakshaya@gmail.com>
2023-12-04 22:30:42 -0800

    edit and view page

--- akshaya_tonde/akshaya_tonde/urls.py
+++ akshaya_tonde/akshaya_tonde/urls.py
@@ -21,9 +21,9 @@ from contact_management.views import *
 
 urlpatterns = [
     path('', home, name= 'Home'),
-    path('/', home, name= 'Home'),
-    path('viewContact/', viewContact, name= 'viewContact'),
+    path('viewContact/<id>/', viewContact, name= 'viewContact'),
     path('createContact/', createContact, name= 'createContact'),
     path('deleteContact/<id>/', deleteContact, name= 'deleteContact'),
+    path('editContact/<id>/', editContact, name= 'editContact'),
     path('admin/', admin.site.urls),
 ]
--- akshaya_tonde/contact_management/views.py
+++ akshaya_tonde/contact_management/views.py
@@ -6,7 +6,6 @@ import django.utils.timezone
 
 def home(request):
     queryset = ContactsInfo.objects.all() 
-
     return render(request, "index.html", context={'page':'Contact Management', 'contactsInfo': queryset})
 
 
@@ -21,18 +20,38 @@ def createContact(request):
             notes = data.get("notes"),
             creation_date = django.utils.timezone.now()
         )
-        return redirect('/viewContact')
-
+        return redirect('/')
+    
+    context={'page':'Create Contact'}
     return render(request, "createContact.html", context)
 
-def viewContact (request):
-
-    queryset = ContactsInfo.objects.all()  
+def viewContact (request, id):
+    queryset = ContactsInfo.objects.get(id=id)
     print(queryset)
     context= {'contactsInfo': queryset, 'page': 'View Contact'}
+
+    if request.method == "POST":
+
+        data = request.POST
+        print(data)
+
+        if 'home' in request.POST:
+            return redirect('/')
+        
+        if 'edit' in request.POST:
+            return redirect('/')
+        
+        if 'cancel' in request.POST:
+            return redirect('/')
+
     return render(request, "viewContact.html", context)
 
 def deleteContact(request, id):
     queryset = ContactsInfo.objects.get(id=id)
     queryset.delete()
     return redirect('/')
+
+def editContact(request, id):
+    queryset = ContactsInfo.objects.get(id=id)
+    context= {'contactsInfo': queryset, 'page' : 'Edit Contact'}
+    return render(request, "editContact.html", context)
